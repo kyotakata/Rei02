b5f40983a1c6993eb8c81f783be08e1b2c341af4
kyotakata <57854222+kyotakata@users.noreply.github.com>
2023-12-21 22:10:40 +0900

    24.C04_コンポジットパターンの実装

---
+++ Rei02/Area/BlockArea.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rei02.Area
+{
+    internal sealed class BlockArea : AreaBase
+    {
+        private List<AreaBase> _areas = new List<AreaBase>();
+        public BlockArea(string name) : base(name)
+        {
+        }
+
+        public override void Add(AreaBase area)
+        {
+            _areas.Add(area);
+        }
+    }
+}
---
+++ Rei02/Area/MeasureArea.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rei02.Area
+{
+    internal sealed class MeasureArea : AreaBase
+    {
+        public MeasureArea(string name) : base(name)
+        {
+        }
+
+        public override void Add(AreaBase area)
+        {
+            throw new ArgumentException("Addはできません");
+        }
+    }
+}
