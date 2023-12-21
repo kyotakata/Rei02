2df5abf981977655c42a3bd56343f774fa745a13
kyotakata <57854222+kyotakata@users.noreply.github.com>
2023-12-21 22:18:54 +0900

    26.C06_階層データの作成

--- Rei02/Form1.cs
+++ Rei02/Form1.cs
@@ -1,3 +1,5 @@
+using Rei02.Area;
+
 namespace Rei02
 {
     public partial class Form1 : Form
@@ -7,6 +9,18 @@ namespace Rei02
             InitializeComponent();
             StartPosition = FormStartPosition.CenterScreen;
             treeView1.HideSelection = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var k = new BlockArea("ŠÖ“Œ");
+            var a = new BlockArea("“Œ‹");
+            var a1 = new MeasureArea("—§ì");
+            var a2 = new MeasureArea("O‘é");
+
+            k.Add(a);
+            a.Add(a1);
+            a.Add(a2);
         }
     }
 }
