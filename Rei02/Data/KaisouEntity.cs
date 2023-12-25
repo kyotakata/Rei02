c0cd73dc2d4795bf091f1332e3910afc6ed045be
kyotakata <57854222+kyotakata@users.noreply.github.com>
2023-12-25 23:03:43 +0900

    36.C16_EntityとFakeデータの作成

---
+++ Rei02/Data/KaisouEntity.cs
@@ -0,0 +1,19 @@
+﻿namespace Rei02.Buhin.Data
+{
+    internal sealed class KaisouEntity
+    {
+        public KaisouEntity(int id, int pearentId, int kind, string name)
+        {
+            Id = id;
+            PearentId = pearentId;
+            Kind = kind;
+            Name = name;
+        }
+
+        public int Id { get; }
+        public int PearentId { get; }
+        public int Kind { get; }
+        public string Name { get; }
+
+    }
+}
